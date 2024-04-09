d7b42170fc3ec78003acc2d4ded716ff9ba1f61e
suedatpc <sueda.tpcu@gmail.com>
2024-04-09 12:34:16 +0300

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,10 @@
+#A tip calculator
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip_percentage = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+
+total_bill = bill + (bill * tip_percentage / 100)
+Each_person = "{:.2f}".format(total_bill / people) #formatting to 2 digits instead of using round()
+print(f"Each person should pay "+ Each_person)
+
