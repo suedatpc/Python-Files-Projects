28654822977242073f0fbe1ff3153a1ab9076265
suedatpc <sueda.tpcu@gmail.com>
2024-04-19 09:12:12 +0300

    cipher program

---
+++ Day8.py
@@ -0,0 +1,50 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def ceaser(start_text, num_of_shift, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        num_of_shift *= -1
+    for i in start_text:
+        if i in alphabet:
+            shifted_index = alphabet.index(i) + num_of_shift
+            new_letter = alphabet[shifted_index]
+            end_text += new_letter
+        else:
+            end_text += i
+    
+    print(f"Here's the {direction}d result: {end_text}")
+
+
+end = False
+while not end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if shift > len(alphabet):
+        shift %= len(alphabet)
+
+    ceaser(start_text=text, num_of_shift=shift, cipher_direction=direction)
+
+    again = input("Type 'yes' if you want to go again. Otherwise type 'no' . \n") . lower()
+    if again == "no":
+        end = True
+        print("Goodbye")
